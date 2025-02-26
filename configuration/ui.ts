fcbfa314f77aa838bbaaa0a1408d90cdefe1e99a
jtbeverly <john.t.beverly@gmail.com>
2025-02-26 13:03:54 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `[Hello SUPPO!]`;
+export const MESSAGE_PLACEHOLDER: string = `[I can help you find answers within Navy and DOD Instructions!]`;
 export const FOOTER_MESSAGE: string = `SUPPO Assistant` ;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `[Clear]`;
+export const PAGE_TITLE: string = `[I find answers in Instruction]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
