655005ebc5504503afe25ebd819aabfec06acd66
jtbeverly <john.t.beverly@gmail.com>
2025-02-26 13:05:05 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[Jack Beverly]`;
-export const OWNER_DESCRIPTION: string = `[MBA Candidate]`;
+export const OWNER_NAME: string = `Jack B's`;
+export const OWNER_DESCRIPTION: string = `MBA Candidate`;
 
-export const AI_NAME: string = `[SUPPO Assistant]`;
-export const AI_TONE: string = `[SUPPO Assistant]`;
-export const AI_ROLE: string = `[Find Answers in Instruction]`;
+export const AI_NAME: string = `SUPPO Assistant`;
+export const AI_TONE: string = `SUPPO Assistant`;
+export const AI_ROLE: string = `I Find Answers in Instruction`;
